fc8625e09626cb6f37820d513d7ac90fc6efb117
Yingjin <yj4@Yings-MacBook-Pro.local>
2020-04-25 13:56:13 -0500

    changes for docker

---
+++ docker-config/redeploy-code.sh
@@ -0,0 +1,9 @@
+docker cp ../dspace/target/dspace-installer/webapps/xmlui riceDspace:/usr/local/tomcat/webapps/ROOT
+docker cp ../dspace/target/dspace-installer/webapps/solr riceDspace:/usr/local/tomcat/webapps/solr
+docker cp ../dspace/target/dspace-installer/webapps/jspui riceDspace:/usr/local/tomcat/webapps/jspui
+docker cp ../dspace/target/dspace-installer/webapps/rest riceDspace:/usr/local/tomcat/webapps/rest
+docker cp ../dspace/target/dspace-installer/webapps/oai riceDspace:/usr/local/tomcat/webapps/oai
+docker cp ../dspace/target/dspace-installer/webapps/rdf riceDspace:/usr/local/tomcat/webapps/rdf
+docker cp ../dspace/target/dspace-installer/webapps/sword riceDspace:/usr/local/tomcat/webapps/sword
+docker cp ../dspace/target/dspace-installer/webapps/swordv2 riceDspace:/usr/local/tomcat/webapps/swordv2
+
